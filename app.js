49440e7e8af7fbe8021d6798302f50326ec6d881
Rony Escobar <et234100256@uma.edu.sv>
2025-03-10 19:40:15 -0600

    Instrucción 'Start' Agrdegada

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 import express from 'express'
 import cors from 'cors'
+import { sequelize } from './config/database.js';
 
 
 const app = express() 
-app.use(cors())
-app.use(json())
+app.use(cors());
+app.use(express.json());
+
+import studentsRoutes from './routes/students.js';
+app.use('/students', studentsRoutes);
 
 const PORT = 3000
-app.listen(PORT, () => {
-    console.log(`Servidor Corriendo en http://localhost:${PORT}`)
+sequelize.sync()
+    .then(() => {
+        console.log('Base de datos sincronizada');
+app.listen(PORT, () =>  console.log(`Servidor Corriendo en http://localhost:${PORT}`))
 })
+.catch(err => console.error('Errior al sincronizar con la base de datos:', err));
